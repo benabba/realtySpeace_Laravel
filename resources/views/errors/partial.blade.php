e4d342cb9adb234372b10b5378248953dd4da827
benabba <ei_ben_abba@esi.dz>
2019-04-21 14:51:53 +0100

    fffff

--- resources/views/errors/partial.blade.php
+++ resources/views/errors/partial.blade.php
@@ -2,6 +2,8 @@
 <p class="lead">{{ __('errors.error-' . $number . '-info') }}</p>
 @if($number != '503')
     <p class="lead">
+        
+        
         <a href="{{ url('/') }}" class="btn btn-default">{{ __('Home') }}</a>
     </p>
 @endif
