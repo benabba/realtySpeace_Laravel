b47f4228f5056408d0aaff34c769f7f5af207173
benabba <ei_ben_abba@esi.dz>
2019-04-21 12:57:25 +0100

    rrr

--- resources/views/auth/dcvjsk.php
+++ resources/views/auth/dcvjsk.php
@@ -5,23 +5,28 @@
         <div class="row">
             <div class="col-twelve">
                 <div class="primary-content">
-                    @if (session('status'))
+                    @if (session('confirmation-success'))
                         @component('front.components.alert')
                             @slot('type')
                                 success
                             @endslot
-                            <p>{{ session('status') }}</p>
+                            {!! session('confirmation-success') !!}
                         @endcomponent
                     @endif
-                    <h3>@lang('Reset Password')</h3>
-                    <form role="form" method="POST" action="{{ route('password.request') }}">
+                    <h3>@lang('Register')</h3>
+                    <form role="form" method="POST" action="{{ route('register') }}">
                         {{ csrf_field() }}
-                        <input type="hidden" name="token" value="{{ $token }}">
+                        @if ($errors->has('name'))
+                            @component('front.components.error')
+                                {{ $errors->first('name') }}
+                            @endcomponent
+                        @endif 
+                        <input id="name" placeholder="@lang('Name')" type="text" class="full-width"  name="name" value="{{ old('name') }}" required autofocus>
                         @if ($errors->has('email'))
                             @component('front.components.error')
                                 {{ $errors->first('email') }}
                             @endcomponent
-                        @endif                          
+                        @endif                       
                         <input id="email" placeholder="@lang('Email')" type="email" class="full-width"  name="email" value="{{ old('email') }}" required>
                         @if ($errors->has('password'))
                             @component('front.components.error')
@@ -30,7 +35,7 @@
                         @endif 
                         <input id="password" placeholder="@lang('Password')" type="password" class="full-width"  name="password" required>
                         <input id="password-confirm" placeholder="@lang('Confirm your password')" type="password" class="full-width" name="password_confirmation" required>
-                        <input class="button-primary full-width-on-mobile" type="submit" value="@lang('Reset Password')">
+                        <input class="button-primary full-width-on-mobile" type="submit" value="@lang('Register')">
                     </form>
                 </div>
             </div>
