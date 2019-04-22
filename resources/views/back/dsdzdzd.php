523f90e0d064ee2d4e268f5205f979757868caba
benabba <ei_ben_abba@esi.dz>
2019-04-22 21:40:15 +0100

    ggggg

--- resources/views/back/dsdzdzd.php
+++
@@ -1,9 +0,0 @@
-@extends('back.layout')
-
-@section('main')
-    @admin
-        <div class="row">
-            @each('back/partials/pannel', $pannels, 'pannel')
-        </div>
-    @endadmin
-@endsection
